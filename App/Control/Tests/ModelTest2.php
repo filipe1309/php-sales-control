bb42fe8c0c495ea99c64c6250c74d5b4fb9d1fa6
filipe1309 <filipe1309@outlook.com>
2017-07-13 02:43:54 +0000

    Add tests for Aggregation between Person and Group

---
+++ App/Control/Tests/ModelTest2.php
@@ -0,0 +1,36 @@
+<?php
+// 7 Creating an app
+// 7.3 Model Classes
+// 7.3.2 Testing Model Classes
+// 7.3.2.2 Aggregation between Person and Group
+
+
+use Book\Control\Page;
+use Book\Database\Transaction;
+
+class ModelTest2 extends Page {
+    public function show()
+    {
+        try {
+            Transaction::open('book');
+            
+            $person1 = Person::find(1);
+            $person1->delGroups();
+            $person1->addGroup(new Group(1));
+            $person1->addGroup(new Group(3));
+            
+            $groups = $person1->getGroups();
+            
+            if ($groups) {
+                foreach ($groups as $group) {
+                    print $group->id . ' - ';
+                    print $group->name . '<br>';
+                }
+            }
+            
+            Transaction::close();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
